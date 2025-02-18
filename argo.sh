6ed55ed524f7d0f9e217dd73ebde3c4ba136fcc6
PavaniVemasani18 <vemasani.pavani@gmail.com>
2025-02-18 15:11:46 +0530

    pod

--- argo.sh
+++ argo.sh
@@ -13,7 +13,7 @@ fi
 if [ "$2" == "jobs" ]; then
     argocd login $(kubectl get svc argocd argocd-server | awk '{print$4}' | tail -1) --username admin --password $(argocd admin initial-password -n argocd | head -1) --insecure --grpc-web
     for app in backend frontend ; do
-    argocd app create ${app} --repo https://github.com/devps23/expense-helm-chart.git --path chart --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/${app}.yml
+    argocd app create ${app} --repo https://github.com/devps23/expense-helm-argocd.git --path chart --dest-server https://kubernetes.default.svc --dest-namespace default.svc --grpc-web --values values/${app}.yml
     done
 fi
 
